82e4ffd84036ac196b88aa660e1a8de13d5f71c8
uniwork <88269554+uniwork-jp@users.noreply.github.com>
2025-06-15 19:03:20 +0900

    Create Stamps UI

---
+++ src/app/rally/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { StampRally } from "@/app/_components/StampRally";
+
+export default function RallyPage() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <StampRally />
+    </main>
+  );
+} 
